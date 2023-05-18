56b71d3328ce7264e40b862a9d593881fc60bdbf
eugene kimani <ekiukkim90@gmail.com>
2023-05-18 14:48:06 -0700

    have been updated

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # simple_shell
 this is an alx group shell prpject by Kimani and Padi
+some of the files included are the shell.h file while contains all the header files and prototypes
---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <signal.h>
+
+#endif
